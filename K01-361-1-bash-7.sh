50f94dd1dd60548d80fda98affb4039da027bf83
Julie Airin <arin-akira@mail.ru>
2014-11-18 12:25:20 +0400

    Добавлены кавычки для красоты. Обнаружена проблема с определением основной группы.

--- K01-361-1-bash-7.sh
+++ K01-361-1-bash-7.sh
@@ -5,8 +5,8 @@ echo ""
 echo "Разработчик: Аринчёхина Юлия"
 cont="y"
 >us_ex
-until [[ -s us_ex || $cont = "n" ]]; do 
-	if [ $cont = "y" ]; then
+until [[ -s us_ex || "$cont" = "n" ]]; do 
+	if [ "$cont" = "y" ]; then
 		echo ""
 		echo "Введите имя пользователя:"
 		read us_name
@@ -20,7 +20,7 @@ until [[ -s us_ex || $cont = "n" ]]; do
 		read cont
 	fi
 done
-if [ ! $cont = "n" ]; then
+if [ ! "$cont" = "n" ]; then
 	cut -d: -f1,4 /etc/group | grep $us_name | cut -d: -f1 >group_list
 	group_main_num=$(cut -d: -f1,4 /etc/passwd | grep $us_name | cut -d: -f2) #Запись в переменную
 	cut -d: -f1,3 /etc/group | grep ^$group_main_num$ | cut -d: -f1 >group_main
