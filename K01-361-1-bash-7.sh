96cb43513c7b09589e8bf223708812c6aa865f3d
Julie Airin <arin-akira@mail.ru>
2014-11-24 20:46:03 +0400

    Определение главной группы исправлено. Нельзя использовать при этом переменные. Завершено.

--- K01-361-1-bash-7.sh
+++ K01-361-1-bash-7.sh
@@ -21,10 +21,13 @@ until [[ -s us_ex || "$cont" = "n" ]]; do
 	fi
 done
 if [ ! "$cont" = "n" ]; then
+	>group_list; >group_main;
 	cut -d: -f1,4 /etc/group | grep $us_name | cut -d: -f1 >group_list
 	group_main_num=$(cut -d: -f1,4 /etc/passwd | grep $us_name | cut -d: -f2) #Запись в переменную
-	cut -d: -f1,3 /etc/group | grep ^$group_main_num$ | cut -d: -f1 >group_main
-	diff group_main group_list >group_nmain_list
+	touch group_main_num_grep
+	group_main_num_grep=':'${group_main_num}
+	cut -d: -f1,3 /etc/group | grep $group_main_num_grep | cut -d: -f1 >group_main
+	diff group_main group_list >group_nmain_list 	
 	>group_nmain_list_res
 	cut -s -d' ' -f2 group_nmain_list >group_nmain_list_res
 	echo ""
